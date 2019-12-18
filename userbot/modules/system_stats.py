fa86101d5fe87c281aa21b55c53b844c15d65c32
ToniStark | 미나 <faridafif1540@gmail.com>
2019-12-18 21:07:59 +0700

    Change: userbot alive info

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -123,20 +123,15 @@ async def pipcheck(pip):
 async def amireallyalive(on):
     """ For .on command, check if the bot is running.  """
     await on.edit("`"
-                     "i am ON My Mastor \n\n"
-                     " \n\n"
-                     "botdo is ON \n\n"
+                     "안녕하세요 \n\n"
+                     " \n"
+                     "Bot is running \n"
                      f"Telethon version: {version.__version__} \n"
                      f"Python: {python_version()} \n"
-                     f"------------------------------------ \n"
+
                      f"User: {DEFAULTUSER} \n"
-                     " \n\n"
-                     f"Creator: Mayur Karaniya \n"
-                     " \n\n"
-                     f"Owner: 3Cube TeKnoways \n"
-                     " \n\n"
-                     f"Userbot: testuserbot "
-                     "`")    
+                     " \n"
+                 "`")    
 
 
 
