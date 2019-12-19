f260baa325fe65d749a8a25972203e82c341fb43
Mr.Miss | 사나 ❤️ <57321015+keselekpermen69@users.noreply.github.com>
2019-12-19 15:23:48 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -122,21 +122,11 @@ async def pipcheck(pip):
 @register(outgoing=True, pattern="^.on$")
 async def amireallyalive(on):
     """ For .on command, check if the bot is running.  """
-    await on.edit("`"
-                     "i am ON My Mastor \n\n"
-                     " \n\n"
-                     "botdo is ON \n\n"
-                     f"Telethon version: {version.__version__} \n"
-                     f"Python: {python_version()} \n"
-                     f"------------------------------------ \n"
-                     f"User: {DEFAULTUSER} \n"
-                     " \n\n"
-                     f"Creator: Mayur Karaniya \n"
-                     " \n\n"
-                     f"Owner: 3Cube TeKnoways \n"
-                     " \n\n"
-                     f"Userbot: testuserbot "
-                     "`")    
+    await on.edit(" Hi I'm Alive Master :*\n"
+                  " Hope your days great \n"
+                  f"~ Telethon ver : {version.__version__} \n"
+                  f"~ Python ver  : {python_version()} \n\n"
+                  f"~ {DEFAULTUSER} ~")
 
 
 
