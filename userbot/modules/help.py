b7ed70610e6ed49446259f70e677dea6a1962c5d
Mr.Miss | 사나 ❤️ <57321015+keselekpermen69@users.noreply.github.com>
2019-12-22 16:36:22 +0700

    .help format back to stock format

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -24,5 +24,5 @@ async def help(event):
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`   |💝💝|   "
+            string += "`\n"
         await event.reply(string)
