9baa871bed91cba6e376223b4be0210216d2abda
ToniStark | 미나 <faridafif1540@gmail.com>
2019-12-23 09:16:30 +0700

    Update: add genius code

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -110,6 +110,9 @@ YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
 # Default .alive name
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
 
+# Genius API key
+GENIUS_API_TOKEN = os.environ.get("GENIUS_API_TOKEN", None)
+
 # Time & Date - Country and Time Zone
 COUNTRY = str(os.environ.get("COUNTRY", ""))
 TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
