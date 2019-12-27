1aa7462c6f71aac4bc5bb8c280a0a05b34eef177
Mr.Miss | 사나 ❤️ <57321015+keselekpermen69@users.noreply.github.com>
2019-12-27 17:05:09 +0700

    edit the PM unapproved msg

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -19,8 +19,9 @@ from userbot.events import register
 UNAPPROVED_MSG = (
     "`HeY! This is an automated message.\n\n`"
     "`I haven't approved you to PM yet.`"
-    "`Please wait for me to look in, I mostly approve PMs.\n\n`"
-    "`Until then, please don't spam my Mastor's PM, you'll get blocked and reported if you do so!`")
+    "`Please wait for me to look in, I mostly approve PMs.`"
+    "`Until then, please don't spam Mr.Miss's PM, you'll automatically get blocked and reported if you do so!\n\n`"
+    "`❤️사나❤️`"  )
 # =================================================================
 
 
