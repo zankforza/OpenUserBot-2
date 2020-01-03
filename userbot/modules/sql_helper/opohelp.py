4e9fa792cc6c8d481cfdc222d43b99febecf89fe
ToniStark | 미나 <faridafif1540@gmail.com>
2020-01-04 06:49:20 +0700

    Change: itulah pokoknya

--- userbot/modules/sql_helper/opohelp.py
+++ userbot/modules/sql_helper/opohelp.py
@@ -5,7 +5,7 @@
 #
 """ Module that holding database components """
 
-from userbot import DB_URI
+from userbot import DONGO, REDIS
 
 
 # Spotify
@@ -13,37 +13,37 @@ from userbot import DB_URI
 
 async def sfsetartist(artist):
     """ Set Spotify Artist """
-    DB_URI.set('sfartist', artist)
+    REDIS.set('sfartist', artist)
 
 
 async def sfsetsong(song):
     """ Set Spotify Song """
-    DB_URI.set('sfsong', song)
+    REDIS.set('sfsong', song)
 
 
 async def spotifycheck(spotifychck):
     """ Set Spotify Check (Status) """
-    DB_URI.set('spotifycheck', spotifychck)
+    REDIS.set('spotifycheck', spotifychck)
 
 
 async def exceptionexist(olexception):
     """ Set if Spotify had exception before """
-    DB_URI.set('exceptionexist', olexception)
+    REDIS.set('exceptionexist', olexception)
 
 
 async def sfgetsong():
     """ Get Spotify Song """
-    return DB_URI.get('sfsong').decode("UTF-8")
+    return REDIS.get('sfsong').decode("UTF-8")
 
 
 async def sfgetartist():
     """ Get Spotify Artist """
-    return DB_URI.get('sfartist').decode("UTF-8")
+    return REDIS.get('sfartist').decode("UTF-8")
 
 
 async def getexception():
     """ Get if Spotify had exception before """
-    exceptcheck = DB_URI.get('exceptionexist')
+    exceptcheck = REDIS.get('exceptionexist')
     if exceptcheck is True:
         return True
 
@@ -52,7 +52,7 @@ async def getexception():
 
 async def getspotifycheck():
     """ Get Spotify Check (Status) """
-    spotifychk = DB_URI.get('spotifycheck')
+    spotifychk = REDIS.get('spotifycheck')
     if spotifychk is True:
         return True
 
